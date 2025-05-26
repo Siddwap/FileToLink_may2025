82e3f67d3822e317e9081d4e45d48bcf7f29e8af
Siddwap <58450503+Siddwap@users.noreply.github.com>
2025-05-26 10:56:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,7 +23,7 @@ else:
 URL = environ.get("URL", "")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001911169401'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001161191495'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1958355347').split()]
 
 # MongoDB information
